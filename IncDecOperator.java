d92e1ace0f34a144629f35420eeb584df85b6de7
sufyan9113 <shaikhsufyan000@gmail.com>
2024-04-22 23:37:30 +0530

    change

--- IncDecOperator.java
+++ IncDecOperator.java
@@ -2,10 +2,18 @@ public class IncDecOperator
 {
     public static void main(String[] args) 
     {
-        int a=10;
-        int b=5;
+        int a=5;
+        int b;
 
-        a+=1;
+     // a   6-   7  -    8       9   8       7
+     //b    5    7       8       8   8       8   
+        b= a++ + ++a + ++a + a++ + --a + a--;
+
         System.out.println(a);
+        System.out.println(b);
+
+        
+
+       
     }
 }
