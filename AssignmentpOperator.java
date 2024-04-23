0c30a1921c4e9627be26693a89843668a9e2d082
sufyan9113 <shaikhsufyan000@gmail.com>
2024-04-23 20:08:11 +0530

    relational

--- AssignmentpOperator.java
+++ AssignmentpOperator.java
@@ -6,5 +6,13 @@ public class AssignmentpOperator
         a=b=c=d=10; //chain assignment
 
         System.out.println(a+" "+b+" "+c+" "+d);
+
+        int e=0; int f=5;
+
+        e+=5; //compoud assignment // e=e+5;
+        System.out.println(e);
+
+        f-=3;
+        System.out.println(f);
     }   
 }
---
+++ Relational.java
@@ -0,0 +1,32 @@
+public class Relational 
+{
+    public static void main(String[] args) 
+    {
+        int a=5; int b=8; // relation operators return boolean output true or false  
+                          // > greater than < less than == equal >= greater or equal <= less than or equal != not equal
+
+
+           boolean check= a>b;
+           System.out.println(check);
+
+           boolean check1= a<b;
+           System.out.println(check1);
+
+           boolean check2= a==b;
+           System.out.println(check2);
+
+           boolean check5= a!=b;
+           System.out.println(check5);
+
+           boolean check3= a>=b;
+           System.out.println(check3);
+
+           boolean check4= a<=b;
+           System.out.println(check4);
+           
+
+
+
+    }
+    
+}
