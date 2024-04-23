f65846db070d3b7aaf1bde9da9c38636bac85c8c
sufyan9113 <shaikhsufyan000@gmail.com>
2024-04-23 19:58:01 +0530

    assignment operator

---
+++ AssignmentpOperator.java
@@ -0,0 +1,10 @@
+public class AssignmentpOperator 
+{
+    public static void main(String[] args) 
+    {
+        int a,b,c,d;
+        a=b=c=d=10; //chain assignment
+
+        System.out.println(a+" "+b+" "+c+" "+d);
+    }   
+}
